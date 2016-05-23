8f7e1526bed42b4dfdafcf56a7af16bad216cc6d
firegreen <steeve.v91@gmail.com>
2016-05-23 15:45:59 +0200

    Add Controller and Model.

---
+++ WWW/index.php
@@ -0,0 +1,16 @@
+<?php
+session_start();
+require_once("./Model/MainModel.php");
+require_once("./Controller/MainController.php");
+$w = new Tinggi(".","Page Test");
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+	<?php $w->buildHeader(); ?>
+	</head>
+	<body onresize="resize()">
+		<div class="bloc_page">Kikou
+		</div>
+	</body>
+</html>
---
+++ WWW/model/MainModel.php
@@ -0,0 +1,29 @@
+<?php
+  class Tinggi{
+
+    private $title;
+    private $icon;
+    private $dir;
+    private $enconding;
+    private $style;
+
+    public function __construct($dir, $title){
+      $this->dir = $dir;
+      $this->title = $title;
+      $this->encoding = "UTF-8";
+      $this->icon = "tinggi.png";
+      $this->style = "main.css";
+    }
+
+    public function buildHeader(){
+        echo
+        "<link rel=\"stylesheet\" href=\"$this->dir/css/$this->style\" />
+        <link rel=\"icon\" type=\"image/x-png\" href=\"$this->dir/data/img/$this->icon\" />
+        <meta charset=\"$this->encoding\"/>
+        <title>
+        $this->title
+        </title> ";
+    }
+
+  }
+ ?>

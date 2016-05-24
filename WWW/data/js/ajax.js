42e9536f68e2220ef56c264c8407e6af341f00b9
Celine NOEL <laefy22@gmail.com>
2016-05-24 16:30:56 +0200

    [ADD] Functions to manage likes / dislikes in JS.

---
+++ WWW/data/js/ajax.js
@@ -0,0 +1,11 @@
+function ajaxRequest(url, action) {
+	var request = new XMLHttpRequest();
+	request.onreadystatechange = function() {
+		if (request.readyState == 4 && request.status == 200) {
+			action(request.responseText);
+		}
+	};
+
+	request.open("GET", url, true);
+	request.send();
+}
---
+++ WWW/data/js/like.js
@@ -0,0 +1,21 @@
+function toggleLike(element) {
+	var id = element.getAttribute("data-id");
+	var parameter = JSON.parse('{ "id":' + id + " }");
+
+	var action = function(response) {
+		// Upgrade number of like in the view.
+	} 
+
+	ajaxRequest('Post/toggleLike/' + parameter);
+}
+
+function toggleDislike(element) {
+	var id = element.getAttribute("data-id");
+	var parameter = JSON.parse('{ "id":' + id + " }");
+
+	var action = function(response) {
+		// Upgrade number of dislike in the view.
+	} 
+
+	ajaxRequest('Post/toggleDislike/' + parameter);
+}

8794bd5628934c091779876e4acf81041f8e0d90
Grégoire <gregoire@ordinateur.local>
2016-05-25 17:00:02 +0200

    views

---
+++ WWW/view/read.view.php
@@ -0,0 +1,44 @@
+<header>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="intro-text">
+                  <span class="name">Article</span>
+                  <span class="desc">texte</span>
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
+<section id="view" class="container-fluid">
+  <div class="row">
+    <div class="col-xs-10 col-xs-offset-1">
+      <div class="col-md-8 col-md-offset-2">
+        <div class="media">
+          <div class="media-left">
+            <a href="#">
+              <!--<img class="media-object" src="..." alt="...">-->
+            </a>
+          </div>
+          <div class="media-body">
+            <h6 class="media-heading">Author</h6>
+            un super commentaire
+          </div>
+        </div>
+        <div class="media">
+          <div class="media-left">
+            <a href="#">
+              <!--<img class="media-object" src="..." alt="...">-->
+            </a>
+          </div>
+          <div class="media-body">
+            <h6 class="media-heading">moi</h6>
+            <div class="form-group">
+              <textarea class="form-control"></textarea>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>

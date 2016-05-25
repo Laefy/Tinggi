6bb5cb73104fb2a24d1882eb699d2114a9f964d1
Grégoire <gregoire@ordinateur.local>
2016-05-25 10:59:52 +0200

    [ADD] Renderer

--- WWW/view/Renderer.php
+++ WWW/view/Renderer.php
@@ -1,8 +1,31 @@
 <?php
 class Renderer{
 
-  public static function render404() {
+  private static function render_header(){
+    self::render_one('common/header.view.php');
+  }
 
+  private static function render_footer(){
+    self::render_one('common/header.view.php');
+  }
+
+  private static function render_nav(){
+    self::render_one('common/nav.view.php');
+  }
+
+  public static function render_one($view){
+    include 'view/'.$view;
+  }
+
+  public static function render($view){
+    self::render_header();
+    self::render_nav();
+    self::render_one($view);
+    self::render_footer();
+  }
+
+  public static function render404() {
+    self::render('common')
   }
 
   public static function renderJson($data) {

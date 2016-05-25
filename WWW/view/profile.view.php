c237218bc93ade8c4a27ba581dcbb469eedfa6e1
Mélodie Mersch <melodie.mersch@gmail.com>
2016-05-25 17:44:19 +0200

    [UPD] modif du dernier champ : confirmation mdp

--- WWW/view/profile.view.php
+++ WWW/view/profile.view.php
@@ -25,7 +25,7 @@
                 <input type="password" class="form-control" id="password" placeholder="mot de passe">
               </div>
               <div class="form-group">
-                <input type="login" class="form-control" id="login" placeholder="pseudo">
+                <input type="password" class="form-control" id="password" placeholder="confirmation du mot de passe">
               </div>
               <div class="form-group">
                 <label for="img">Choissisez une image de profil</label>

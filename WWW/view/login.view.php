c63f30c90aead93b462515de0b5e8aa948836aae
Grégoire <gregoire@ordinateur.local>
2016-05-25 15:14:41 +0200

    main views

---
+++ WWW/view/login.view.php
@@ -0,0 +1,29 @@
+<header>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="intro-text">
+                  <span class="name">Salut !</span>
+                  <span class="desc">Saisissez votre identifiant et votre mot de passe ! :)</span>
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
+<section id="view" class="container-fluid">
+  <div class="container-fluid">
+      <div class="row">
+          <div class="col-md-4 col-md-offset-4">
+            <form>
+              <div class="form-group">
+                <input type="email" class="form-control" id="email" placeholder="email">
+              </div>
+              <div class="form-group">
+                <input type="password" class="form-control" id="password" placeholder="mot de passe">
+              </div>
+              <button type="submit" class="btn btn-default">C'est moi !</button>
+            </form>
+          </div>
+      </div>
+  </div>
+</section>
---
+++ WWW/view/tops.view.php
@@ -0,0 +1,62 @@
+<header>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="intro-text">
+                  <span class="name">Les TOPs des TOPs</span>
+                  <span class="desc">Hum... des classements !</span>
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
+<section id="view" class="container-fluid">
+  <div class="row">
+    <div class="col-lg-12">
+        <div class="intro-text">
+          <span class="name">TOP TINGGI</span>
+          <span class="desc">Les meilleurs publication du site !</span>
+        </div>
+    </div>
+    <div class="col-xs-10 col-xs-offset-1">
+      <a href="#" class="thumbnail">
+        <!--<img src="..." alt="...">-->
+        <div class="caption">
+          <h3>post 1</h3>
+          <p>description</p>
+        </div>
+      </a>
+      <a href="#" class="thumbnail">
+        <!--<img src="..." alt="...">-->
+        <div class="caption">
+          <h3>post 2</h3>
+          <p>description</p>
+        </div>
+      </a>
+    </div>
+
+    <div class="col-lg-12">
+        <div class="intro-text">
+          <span class="name">TOP UTILISATEUR</span>
+          <span class="desc">Vos meilleurs publications !</span>
+        </div>
+    </div>
+
+    <div class="col-xs-10 col-xs-offset-1">
+      <a href="#" class="thumbnail">
+        <!--<img src="..." alt="...">-->
+        <div class="caption">
+          <h3>post 1</h3>
+          <p>description</p>
+        </div>
+      </a>
+      <a href="#" class="thumbnail">
+        <!--<img src="..." alt="...">-->
+        <div class="caption">
+          <h3>post 2</h3>
+          <p>description</p>
+        </div>
+      </a>
+    </div>
+  </div>
+</section>

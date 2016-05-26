a00bcd6762fa6cd9179577935264d5de1071d7ac
Mélodie Mersch <melodie.mersch@gmail.com>
2016-05-26 10:59:06 +0200

    [ADD] getTopTen in User

--- WWW/model/Post.php
+++ WWW/model/Post.php
@@ -72,7 +72,7 @@ class Post {
         $posts = array();
 
         foreach ($rows as $row) {
-            array_push(posts,postFromRow($row));
+            array_push(posts, Post::postFromRow($row));
         }
 
         return posts;
